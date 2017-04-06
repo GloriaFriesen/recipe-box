d29f7ffe738cde210f119983177bf3f55d892845
Gloria Friesen and Nathaniel Meyer <gloria@>
2017-04-06 11:59:58 -0700

    Add form and post route to add new tags to recipe

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -37,8 +37,12 @@ public class App {
       Recipe recipe = Recipe.find(Integer.parseInt(request.params(":recipe_id")));
       List<Ingredient> ingredients = recipe.getIngredients();
       List<Instruction> instructions = recipe.getInstructions();
+      List<Tag> recipeTags = recipe.getTags();
+      List<Tag> tags = Tag.all();
       model.put("ingredients", ingredients);
       model.put("instructions", instructions);
+      model.put("recipeTags", recipeTags);
+      model.put("tags", tags);
       model.put("recipe", recipe);
       model.put("template", "templates/recipe.vtl");
       return new ModelAndView(model, layout);
@@ -66,5 +70,21 @@ public class App {
       response.redirect(url);
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    post("recipes/tags/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Integer recipeId = Integer.parseInt(request.queryParams("recipeId"));
+      String tagName = request.queryParams("tagName");
+      Tag tag = new Tag(tagName);
+      tag.save();
+      Recipe recipe = Recipe.find(recipeId);
+      recipe.addTag(tag);
+      String url = String.format("/recipes/%d", recipeId);
+      response.redirect(url);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+  //   post("recipes/tag/addExisting" (request, response))
+
   }
 }
